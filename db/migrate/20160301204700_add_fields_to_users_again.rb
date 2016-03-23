2e0f027c739686feb755280829152c0383a54f3d
natasapetrovic <npetrovic789@yahoo.com>
2016-03-23 09:40:46 +0000

    migracija ispravljena

--- db/migrate/20160301204700_add_fields_to_users_again.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToUsersAgain < ActiveRecord::Migration
-  def change
-    add_column :users, :name, :string
-    add_column :users, :phone, :string
-    add_column :users, :address, :string
-  end
-end
