e97134c8bcba12e85a5d711ac15a7c95cf2223bb
natasapetrovic <npetrovic789@yahoo.com>
2016-04-19 15:17:17 +0000

    uredjenje search-a

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20160308103028) do
     t.integer  "user_id"
   end
 
+  create_table "infos", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "address"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "premium_infos", force: :cascade do |t|
     t.string   "first_name"
     t.datetime "created_at",     null: false
@@ -37,6 +46,38 @@ ActiveRecord::Schema.define(version: 20160308103028) do
     t.string   "website"
     t.integer  "user_id"
   end
+
+  create_table "premium_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "personal_phone"
+    t.string   "personal_email"
+    t.string   "website"
+    t.string   "working_time"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "description"
+  end
+
+  add_index "premium_users", ["email"], name: "index_premium_users_on_email", unique: true
+  add_index "premium_users", ["reset_password_token"], name: "index_premium_users_on_reset_password_token", unique: true
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
